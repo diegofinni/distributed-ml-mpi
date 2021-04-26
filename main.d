492865c355d8d4e3a62b4ae419c2396239adbf2a
Diego San Miguel <dsanmigu@andrew.cmu.edu>
2021-04-25 23:05:10 -0400

    A file was deleted

--- main.d
+++
@@ -1,8 +0,0 @@
-main.o: main.c /usr/local/depot/openmpi-4.0.2/include/mpi.h \
- /usr/local/depot/openmpi-4.0.2/include/mpi_portable_platform.h dml.h
-
-/usr/local/depot/openmpi-4.0.2/include/mpi.h:
-
-/usr/local/depot/openmpi-4.0.2/include/mpi_portable_platform.h:
-
-dml.h:
